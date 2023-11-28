11fe090c659b19e79ba8b69fd5c8e8241553d88a
unknown <martinsogunsina@gmail.com>
2023-11-28 14:37:01 +0600

    react-rtk updates

--- react-rtk-demo/src/App.jsx
+++ react-rtk-demo/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 
 import { CakeView } from "./features/cake/CakeView";
@@ -8,8 +6,6 @@ import { IcecreamView } from "./features/icecream/IcecreamView";
 import { UserView } from "./features/user/UserView";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
